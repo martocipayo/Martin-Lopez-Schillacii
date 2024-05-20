65391dc648a50ff2dbf6e45c7742f622aa953930
martocipayo <162316029+martocipayo@users.noreply.github.com>
2024-05-19 21:15:34 -0300

    Add files via upload

---
+++ CODIGO_del_TP1_Martin_Lopez_Schillaci.ino
@@ -0,0 +1,46 @@
+void setup()
+{
+  pinMode( 10 , OUTPUT );
+  pinMode( 9 , OUTPUT );
+  pinMode( 7 , OUTPUT );
+  pinMode( 5 , OUTPUT );
+  pinMode( 4 , OUTPUT );
+  pinMode( 3 , OUTPUT );
+}
+
+void loop()
+{
+  digitalWrite(7, HIGH);
+  
+  delay(100);
+  
+  digitalWrite(7, LOW);
+  analogWrite(9 , 0);
+  analogWrite(10 , 0);
+  analogWrite(3 , 255);
+  
+  delay(1000);
+  
+  digitalWrite(7, HIGH);
+  
+  delay(100);
+  
+  digitalWrite(7, LOW);
+  analogWrite(3 , 0);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  
+  delay(1000);
+  
+  digitalWrite(7, HIGH);
+  
+  delay(100);
+  
+  digitalWrite(7, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  analogWrite(9 , 255);
+  analogWrite(10 , 255);
+  
+  delay(1000);
+}
